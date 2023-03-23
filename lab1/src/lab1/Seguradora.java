ac21e23dc795e4939eb6a6790f7f20b24841f81d
Francisco Vinicius Sousa Guedes <f260440@dac.unicamp.br>
2023-03-23 14:38:51 -0300

    Aprova pfv

---
+++ lab1/src/lab1/Seguradora.java
@@ -0,0 +1,34 @@
+package lab1;
+
+public class Seguradora {
+	private String nome;
+	private String telefone;
+	private String email;
+	private String endereco;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+}
